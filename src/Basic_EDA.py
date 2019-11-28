ad56303d1aeab895057a76035bab8b03b02b8102
Harsh Shah <h.s.shah1511@gmail.com>
2019-11-28 10:51:50 +0530

    modified code to Basic_EDA file

--- src/Basic_EDA.py
+++ src/Basic_EDA.py
@@ -11,11 +11,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import humanize
 import re
+from sklearn.preprocessing import Imputer, StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder
+from sklearn_pandas import CategoricalImputer
 
-path_invoice = r'E:/Mahindra-Capstone/data/Final_invoice.csv'
-path_JTD = r'E:/Mahindra-Capstone/data/JTD.csv'
-path_customer_data = r'E:/Mahindra-Capstone/data/Customer_Data.xlsx'
-path_plant_master = r'E:/Mahindra-Capstone/data/Plant Master.xlsx'
+path_invoice = r'E:/Mahindra-Capstone/greyatom-capstone/data/Final_invoice.csv'
+path_JTD = r'E:/Mahindra-Capstone/greyatom-capstone/data/JTD.csv'
+path_customer_data = r'E:/Mahindra-Capstone/greyatom-capstone/data/Customer_Data.xlsx'
+path_plant_master = r'E:/Mahindra-Capstone/greyatom-capstone/data/Plant Master.xlsx'
 
 
 # In[2]:
@@ -34,7 +36,7 @@ plant_data_original = pd.read_excel(path_plant_master)
 #print(customer_data_original.info())
 
 
-# In[50]:
+# In[3]:
 
 
 # Function to identify numeric features
@@ -57,8 +59,13 @@ def missing_data(df):
     percent = (round(total * 100 / len(df), 2))
     missing_data = pd.DataFrame({'Total': total,
                                 'Percent': percent})
-    missing_data = missing_data.sort_values(by = 'Percent', ascending=False)
+    missing_data = missing_data.sort_values(by = 'Total', ascending=False)
     return missing_data
+
+def get_missing_cols(missing_data_df):
+    missing_data_df = missing_data_df[missing_data_df['Total'] > 0]
+    missing_data_cols = missing_data_df.index.tolist()
+    return missing_data_cols
 
 #Drop missing_data cols setting thresold 
 def drop_missing(df, missing, value):
@@ -99,7 +106,7 @@ def get_df_name(df):
     return name
 
 
-# In[49]:
+# In[4]:
 
 
 #Customer Data - customer_data_original
@@ -145,7 +152,7 @@ print("Shape:===",customer_data.shape)
 print("===="*30)
 
 
-# In[48]:
+# In[5]:
 
 
 #Invoice Data - invoice_data_original
@@ -192,50 +199,50 @@ print("Shape:===",invoice_data.shape)
 print("===="*30)
 
 
-# In[ ]:
+# In[6]:
 
 
-###############
+# ###############
 
-car_make_count_summary = invoice_data['make'].value_counts()
-print(type(car_make_count_summary))
-print(car_make_count_summary)
-car_make_count_summary = car_make_count_summary[:10, ]
-plt.figure(figsize=(15, 5))
-sns.barplot(car_make_count_summary.index, car_make_count_summary.values, alpha=0.8)
-plt.title('MAKE-wise Car-Counts serviced across the nation')
-plt.ylabel('Number of Occurrences', fontsize=12)
-plt.xlabel('Make of Car', fontsize=12)
-plt.xticks(rotation=45)
-plt.show()
+# car_make_count_summary = invoice_data['make'].value_counts()
+# print(type(car_make_count_summary))
+# print(car_make_count_summary)
+# car_make_count_summary = car_make_count_summary[:10, ]
+# plt.figure(figsize=(15, 5))
+# sns.barplot(car_make_count_summary.index, car_make_count_summary.values, alpha=0.8)
+# plt.title('MAKE-wise Car-Counts serviced across the nation')
+# plt.ylabel('Number of Occurrences', fontsize=12)
+# plt.xlabel('Make of Car', fontsize=12)
+# plt.xticks(rotation=45)
+# plt.show()
 
-##################
-avg_spend_by_car_mfd = invoice_data.groupby(['make', 'total_amt_wtd_tax'])['total_amt_wtd_tax'].mean()
-
-
-avg_spend_by_car_mfd_2 = invoice_data.groupby(['make', 'model'])['total_amt_wtd_tax'].sum()
-avg_spend_by_car_mfd_2 = avg_spend_by_car_mfd_2[:15, ].unstack()
-print(avg_spend_by_car_mfd_2)
-# =============================================================================
-# avg_spend_by_car_mfd_arry = []
-# for val in avg_spend_by_car_mfd_2.values:
-# avg_spend_by_car_mfd_2.values[i] = humanize.intword(avg_spend_by_car_mfd_2.values[i])
-# print(humanize.intword(val))
-# avg_spend_by_car_mfd_arry.append(humanize.intword(val))
-# print(avg_spend_by_car_mfd_arry)
-# =============================================================================
+# ##################
+# avg_spend_by_car_mfd = invoice_data.groupby(['make', 'total_amt_wtd_tax'])['total_amt_wtd_tax'].mean()
 
 
-avg_spend_by_car_mfd_2.plot(kind='bar', subplots=True)
-#sns.barplot(avg_spend_by_car_mfd_2.index, avg_spend_by_car_mfd_2.values, alpha=0.8)
-plt.title('MAKE-wise Servicing cost for Cars')
-plt.ylabel('Total Amt of Servicing', fontsize=12)
-plt.xlabel('Make of Car', fontsize=12)
-plt.xticks(rotation=60)
-plt.show()
+# avg_spend_by_car_mfd_2 = invoice_data.groupby(['make', 'model'])['total_amt_wtd_tax'].sum()
+# avg_spend_by_car_mfd_2 = avg_spend_by_car_mfd_2[:15, ].unstack()
+# print(avg_spend_by_car_mfd_2)
+# # =============================================================================
+# # avg_spend_by_car_mfd_arry = []
+# # for val in avg_spend_by_car_mfd_2.values:
+# # avg_spend_by_car_mfd_2.values[i] = humanize.intword(avg_spend_by_car_mfd_2.values[i])
+# # print(humanize.intword(val))
+# # avg_spend_by_car_mfd_arry.append(humanize.intword(val))
+# # print(avg_spend_by_car_mfd_arry)
+# # =============================================================================
 
 
-# In[107]:
+# avg_spend_by_car_mfd_2.plot(kind='bar', subplots=True)
+# #sns.barplot(avg_spend_by_car_mfd_2.index, avg_spend_by_car_mfd_2.values, alpha=0.8)
+# plt.title('MAKE-wise Servicing cost for Cars')
+# plt.ylabel('Total Amt of Servicing', fontsize=12)
+# plt.xlabel('Make of Car', fontsize=12)
+# plt.xticks(rotation=60)
+# plt.show()
+
+
+# In[7]:
 
 
 customer_data_ = data_trimming(customer_data)
@@ -243,10 +250,10 @@ customer_data_ = customer_data_.drop(columns = ['business_partner'])
 print(customer_data_.head())
 print("...."*30)
 print(customer_data_.tail())
-print(customer_data_['customer_no'].astype(str))
+#print(customer_data_['customer_no'].astype(str))
 
 
-# In[60]:
+# In[8]:
 
 
 invoice_data_ = data_trimming(invoice_data)
@@ -259,40 +266,104 @@ print("...."*30)
 print(invoice_data_.tail())
 
 
-# In[108]:
+# In[9]:
 
 
 from sklearn_pandas import CategoricalImputer
 
 
 #Merging invoice_data_ & customer_data_
-
-#data_merged = pd.merge(invoice_data_, customer_data_, on='customer_no', how='outer')
-#data_merged = data_merged.sort_values(ascending = True, by = 'customer_no')
+print(len(invoice_data_.columns.tolist()) + len(customer_data_.columns.tolist()))
+print("==="*30)
 customer_data_['customer_no'] = customer_data_['customer_no'].astype(str)
 invoice_data_['customer_no'] = invoice_data_['customer_no'].astype(str)
-data_merged = pd.merge(invoice_data_, customer_data_, on='customer_no', how='outer')
+
+data_merged = pd.merge(invoice_data_, customer_data_, on='customer_no', how='left')
 #data_merged_1 = data_merged_1.sort_values(ascending = True, by = 'customer_no')
 
 print(data_merged.head())
 print("...."*30)
 print(data_merged.tail())
 
-#cust_mask = (data_merged['customer_no'] == 492314)
-#print(data_merged[cust_mask])
+
+# In[10]:
 
 
-# In[110]:
+# print("==="*30)
+# print(data_merged[data_merged['customer_no'] == "1"])
+# print("==="*30)
+# print(data_merged.shape)
+# print("==="*30)
 
 
-print(data_merged_1.head())
-print("...."*30)
-print(data_merged_1.tail())
-print("==="*30)
-#cust_mask = data_merged['customer_no'] == 492314
-print(customer_data[customer_data['customer_no'] == 1])
-print("==="*30)
-print(invoice_data_[invoice_data_['customer_no'] == "1"])
-print("==="*30)
-print(data_merged[data_merged['customer_no'] == "1"])
+# In[11]:
+
+
+# print(data_merged.isnull().sum().sort_values(ascending= False))
+# print("==="*30)
+# print(data_merged.isna().sum().sort_values(ascending= False))
+# print("==="*30)
+# print(data_merged[data_merged['city'].isnull()])
+# print("==="*30)
+# print(data_merged[data_merged['pin_code'] == 174303]['area','city','pin_code','customer_no'])
+
+
+# In[12]:
+
+
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.impute import SimpleImputer, MissingIndicator
+from sklearn.pipeline import FeatureUnion, make_pipeline
+
+
+# In[55]:
+
+
+df_missing_data = missing_data(data_merged)
+
+missing_data_cols = get_missing_cols(df_missing_data)
+
+missing_data_cols.remove('city')
+missing_data_cols.remove('area')
+missing_data_cols.remove('regn_no')
+df_merged = data_merged[missing_data_cols]
+df_merged['data_origin']= df_merged['data_origin'].astype(str).map(lambda x: x.lstrip('Z'))
+df_merged.head()
+
+
+# In[63]:
+
+
+le = LabelEncoder()
+len(df_merged.model.unique())
+df_merged['model'] = le.fit_transform(df_merged['model'].astype(str))
+# missing_data_cols
+#df_merged.head()
+
+
+# In[14]:
+
+
+# transformer = FeatureUnion(
+#                            transformer_list=[
+#                            ('features', SimpleImputer(strategy='most_frequent')),
+#                            ('indicators', MissingIndicator())])
+# transformer = transformer.fit(data_merged)
+# results = transformer.transform(data_merged)
+# results.shape
+missing_data_cols
+
+
+# In[71]:
+
+
+imp = IterativeImputer(add_indicator=False, estimator=None,
+                 imputation_order='ascending', initial_strategy='most_frequent',
+                 max_iter=10, max_value=None, min_value=None,
+                 n_nearest_features=1,
+                 random_state=0, sample_posterior=False, tol=0.001,
+                 verbose=0)
+imp.fit_transform(df_merged)  
+df_merged.isnull().sum()
 
